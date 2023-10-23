f673938ae96de3e4b5e360e588eb46d0118ddbea
Rodolpho Coller <rodolpho.coller12@gmail.com>
2023-10-22 22:03:34 -0300

    feat: integrate Timer with user interface

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,35 @@ import RemoveIcon from '@mui/icons-material/Remove';
 import PlayCircleFilledIcon from '@mui/icons-material/PlayCircleFilled';
 import { Slider } from '@mui/material';
 import { useState } from 'react';
+import Timer from './Timer';
 
 function App() {
   const [bpm, setBpm] = useState(40);
+  const timer = new Timer(bpm);
+  let shouldMetronomeStart = true;
+
+  function toggleTimer() {
+    if (shouldMetronomeStart) {
+      timer.start();
+      shouldMetronomeStart = !shouldMetronomeStart;
+    } else  {
+      timer.stop();
+      shouldMetronomeStart = !shouldMetronomeStart;
+    }
+  }
 
   return (
     <div>
       <h1>{ bpm } bpm</h1>
       <div>
-        <RemoveIcon onClick={ () => setBpm(prev =>  prev <= 40 ? 40 : (prev - 1)) }/>
+        <RemoveIcon onClick={ () => setBpm(prev =>  prev <= 40 ? 40 : (prev - 1)) } />
 
         <Slider min={ 40 } max={ 400 } value={ bpm } onChange={ e => { setBpm(e.target.value); } } />
 
-        <AddIcon onClick={ () => setBpm(prev =>  prev >= 400 ? 400 : (prev + 1))}/>
+        <AddIcon onClick={ () => setBpm(prev =>  prev >= 400 ? 400 : (prev + 1))} />
       </div>
 
-      <PlayCircleFilledIcon />
+      <PlayCircleFilledIcon onClick={ () => { toggleTimer(); } } />
 
     </div>
   );
