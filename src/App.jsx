87779a069f996fbb1700f91301b12e34979b357a
Rodolpho Coller <rodolpho.coller12@gmail.com>
2023-10-23 20:28:29 -0300

    style: starting css

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import PlayCircleFilledIcon from '@mui/icons-material/PlayCircleFilled';
 import { Slider } from '@mui/material';
 import { useState } from 'react';
 import Timer from './Timer';
+import './app.css';
 
 function App() {
   const [bpm, setBpm] = useState(40);
@@ -21,12 +22,13 @@ function App() {
   }
 
   return (
-    <div>
+    <div className='main-wrapper'>
       <h1>{ bpm } bpm</h1>
-      <div>
+
+      <div className="bpm-setters-container">
         <RemoveIcon onClick={ () => setBpm(prev =>  prev <= 40 ? 40 : (prev - 1)) } />
 
-        <Slider min={ 40 } max={ 400 } value={ bpm } onChange={ e => { setBpm(e.target.value); } } />
+        <Slider min={ 40 } max={ 400 } value={ bpm } sx={{ width: '70%', margin: '0 20px 0 20px' }} onChange={ e => { setBpm(e.target.value); } } />
 
         <AddIcon onClick={ () => setBpm(prev =>  prev >= 400 ? 400 : (prev + 1))} />
       </div>
