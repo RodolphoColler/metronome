ed399cc4bc6f587ba9579dc2ca72bc9c290c5ee3
Rodolpho Coller <rodolpho.coller12@gmail.com>
2023-11-10 07:30:07 -0300

    set up context provider on main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import MetronomeProvider from './context/MetronomeProvider.jsx';
 
 // eslint-disable-next-line no-undef
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <MetronomeProvider>
+      <App />
+    </MetronomeProvider>
   </React.StrictMode>,
 );
