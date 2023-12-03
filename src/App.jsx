ef8e2568b0a6777e4ec2359930dc3e529de15733
Rodolpho Coller <rodolpho.coller12@gmail.com>
2023-12-03 10:44:28 -0300

    add more instructions

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import Modal from '@mui/material/Modal';
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import PlayCircleFilledIcon from '@mui/icons-material/PlayCircleFilled';
 import { useHotkeys } from 'react-hotkeys-hook';
 import { useContext, useEffect, useState } from 'react';
@@ -58,9 +60,14 @@ function App() {
             <ArrowForwardIcon className='key-wrapper' />
             <p>Left and Right key are used to decrease and increase bpm respectively.</p>
           </div>
+          <div className='instruction-wrapper'>
+            <ArrowDownwardIcon className='key-wrapper' />
+            <ArrowUpwardIcon className='key-wrapper' />
+            <p>Downward and Upward key are used to decrease and increase beats respectively.</p>
+          </div>
         </div>
       </Modal>
-      <div className='modal'>
+      <div className='modal-button'>
         <QuestionMarkIcon onClick={handleOpen}/>
       </div>
       <div className='main-wrapper'>
