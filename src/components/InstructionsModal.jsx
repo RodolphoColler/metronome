c5db2ecf9d59cbae7a846c70d8732ab8811384d8
RodolphoColler <rodolpho.coller12@gmail.com>
2023-12-22 16:50:56 -0300

    slightly change on modal

--- src/components/InstructionsModal.jsx
+++ src/components/InstructionsModal.jsx
@@ -14,24 +14,27 @@ function InstructionsModal() {
 
   return (
     <>
-      <Modal
-    open={open}
-    onClose={handleClose}
-  >
+      <Modal open={open} onClose={handleClose}>
         <div className='modal-wrapper'>
           <h2>Hotkeys</h2>
           <div className='instruction-wrapper'>
-            <ArrowBackIcon className='key-wrapper' />
-            <ArrowForwardIcon className='key-wrapper' />
+            <div className='hotkeys-icon-wraper'>
+              <ArrowBackIcon className='key-wrapper' />
+              <ArrowForwardIcon className='key-wrapper' />
+            </div>
             <p>Left and Right key are used to decrease and increase bpm respectively.</p>
           </div>
           <div className='instruction-wrapper'>
-            <ArrowDownwardIcon className='key-wrapper' />
-            <ArrowUpwardIcon className='key-wrapper' />
+            <div className='hotkeys-icon-wraper'>
+              <ArrowDownwardIcon className='key-wrapper' />
+              <ArrowUpwardIcon className='key-wrapper' />
+            </div>
             <p>Downward and Upward key are used to decrease and increase beats respectively.</p>
           </div>
           <div className='instruction-wrapper'>
-            <SpaceBarIcon className='key-wrapper' />
+            <div className='hotkeys-icon-wraper' >
+              <SpaceBarIcon className='key-wrapper' />
+            </div>
             <p>Space key is used to start and stop metronome.</p>
           </div>
         </div>
