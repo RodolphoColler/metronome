e1437631188f671e03a5ec99c7da7c711f6eb746
RodolphoColler <rodolpho.coller12@gmail.com>
2023-12-27 16:25:43 -0300

    start logic to increment bpm by time

--- src/components/countDown/CountDown.jsx
+++ src/components/countDown/CountDown.jsx
@@ -1,23 +1,34 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
+import MetronomeContext from '../../context/MetronomeContext';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
 
 function CountDown() {
-  let count = 60;
-  const [timer, setTimer] = useState('1:00');
-  const [isTimerStarted, setIsTimerStarted] = useState(false);
+  const { increaseBpm } = useContext(MetronomeContext);
+  let count = 5;
+  const [complementBpm, setComplementBpm] = useState(1);
+  const [countDown, setCountDown] = useState('0:05');
+  const [isCountDownStarted, setIsCountDownStarted] = useState(false);
 
   useEffect(() => {
-    if(isTimerStarted) {
+    if(isCountDownStarted) {
       setInterval(() => {
         count-=1;
-        setTimer(`${String(Math.floor(count / 60)).padStart(2, '0')}:${String(count % 60).padStart(2, '0')}`);
+        if (count === 0) {
+          increaseBpm();
+        }
+        setCountDown(`${String(Math.floor(count / 60)).padStart(2, '0')}:${String(count % 60).padStart(2, '0')}`);
       }, 1000);
     }
-  }, [isTimerStarted]);
+  }, [isCountDownStarted]);
 
   return (
     <>
-      <h2>{ timer }</h2>
-      <button onClick={ () => {setIsTimerStarted(prev => !prev);}}>start count down</button>
+      <RemoveIcon sx={{ fontSize: '5rem' }} onClick={ () => setComplementBpm(prev =>  prev <= 60 ? (prev - 1) : 60)} />
+      <p>{ complementBpm }</p>
+      <AddIcon sx={{ fontSize: '5rem' }} onClick={ () => setComplementBpm(prev =>  prev >= 400 ? 400 : (prev + 1)) } />
+      <h2>{ countDown }</h2>
+      <button onClick={ () => {setIsCountDownStarted(prev => !prev);}}>start count down</button>
     </>
   );
 }
