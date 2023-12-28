4159688c67a040d6196e0f32e3bae4718aaa8363
RodolphoColler <rodolpho.coller12@gmail.com>
2023-12-28 09:24:00 -0300

    improving countDown

--- src/components/countDown/CountDown.jsx
+++ src/components/countDown/CountDown.jsx
@@ -3,24 +3,32 @@ import MetronomeContext from '../../context/MetronomeContext';
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 
+const initialCount = 10;
+
 function CountDown() {
   const { increaseBpm } = useContext(MetronomeContext);
-  let count = 5;
+  let count = initialCount;
+  const [intervalCountDown, setIntervalCountdown] = useState();
   const [complementBpm, setComplementBpm] = useState(1);
-  const [countDown, setCountDown] = useState('0:05');
+  const [countDown, setCountDown] = useState('00:10');
   const [isCountDownStarted, setIsCountDownStarted] = useState(false);
 
   useEffect(() => {
     if(isCountDownStarted) {
-      setInterval(() => {
+      setIntervalCountdown(setInterval(() => {
         count-=1;
         if (count === 0) {
-          increaseBpm();
+          increaseBpm(complementBpm);
+          count = initialCount;
         }
         setCountDown(`${String(Math.floor(count / 60)).padStart(2, '0')}:${String(count % 60).padStart(2, '0')}`);
-      }, 1000);
+      }, 1000));
     }
-  }, [isCountDownStarted]);
+    else {
+      clearTimeout(intervalCountDown);
+      setCountDown(`${String(Math.floor(initialCount / 60)).padStart(2, '0')}:${String(initialCount % 60).padStart(2, '0')}`);
+    }
+  }, [isCountDownStarted, ]);
 
   return (
     <>
@@ -28,7 +36,7 @@ function CountDown() {
       <p>{ complementBpm }</p>
       <AddIcon sx={{ fontSize: '5rem' }} onClick={ () => setComplementBpm(prev =>  prev >= 400 ? 400 : (prev + 1)) } />
       <h2>{ countDown }</h2>
-      <button onClick={ () => {setIsCountDownStarted(prev => !prev);}}>start count down</button>
+      <button onClick={ () => { setIsCountDownStarted(prev => !prev); }}>start count down</button>
     </>
   );
 }
